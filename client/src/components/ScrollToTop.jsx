743ee9ccebd723ebe07e5e64bff0aa2637c91764
islam fouad <fouadeslam49@gmail.com>
2026-07-13 13:01:22 +0300

    ققق

--- client/src/components/ScrollToTop.jsx
+++ client/src/components/ScrollToTop.jsx
@@ -1,7 +1,12 @@
-import { useEffect } from 'react';
+import { useLayoutEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+
 export default function ScrollToTop() {
-  const { pathname } = useLocation();
-  useEffect(() => window.scrollTo({ top: 0, behavior: 'smooth' }), [pathname]);
+  const { pathname, search } = useLocation();
+
+  useLayoutEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+  }, [pathname, search]);
+
   return null;
 }
