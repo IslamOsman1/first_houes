c7e7b762277400b2e52deab97eccf355c399bd07
islam fouad <fouadeslam49@gmail.com>
2026-07-13 13:04:55 +0300

    ررر

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,11 @@ import App from './App';
 import { SiteProvider } from './context/SiteContext';
 import './styles.css';
 
+if (!window.location.hash && window.location.pathname !== '/') {
+  const target = `/#${window.location.pathname}${window.location.search}`;
+  window.history.replaceState(null, '', target);
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
